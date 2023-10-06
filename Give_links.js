f0e8cb4dbfb777eb751f91e6ccdc36f358fc7a36
Bhumik Joshi <92202172+bhumik1310@users.noreply.github.com>
2023-10-06 12:40:08 +0530

    Initial Give_Links Script

---
+++ Give_links.js
@@ -0,0 +1,22 @@
+import { Dataset,CheerioCrawler } from 'crawlee';
+// const process = require('process');
+import process from 'process';
+var website = process.argv[2];
+const crawler = new CheerioCrawler({
+    async requestHandler({ request, enqueueLinks, log , body}) {
+        log.info(request.url);
+        await Dataset.pushData({
+            url: request.url,
+            
+        });
+        // Add all links from page to RequestQueue
+        await enqueueLinks({
+            strategy:'same-origin'
+        });
+    },
+    maxRequestsPerCrawl: 2000, // Limitation for only 10 requests (do not use if you want to crawl all links)
+});
+
+// Run the crawler with initial request
+await crawler.run([website]);
+await Dataset.exportToCSV('OUTPUT', { toKVS: 'my-data' });
